95773ca1df6d52789dc463758683501b62f29d0a
hecj <275070023@qq.com>
2014-11-01 15:01:25 +0800

    ddd

---
+++ hecjlucene-core/src/test/java/org/test/hecjlucene/core03/IndexWriterUtilTest.java
@@ -0,0 +1,15 @@
+package org.test.hecjlucene.core03;
+
+import org.junit.Test;
+
+public class IndexWriterUtilTest {
+	
+	
+	@Test
+	public void indexTest(){
+	
+		IndexWriterUtil indexWriterUtil = new IndexWriterUtil();
+		indexWriterUtil.index();
+		
+	}
+}
