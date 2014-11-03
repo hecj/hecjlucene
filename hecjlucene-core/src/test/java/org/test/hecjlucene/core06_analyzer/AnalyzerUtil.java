158a46e14089760e3a46e34d636623ffc904cf42
hecj <275070023@qq.com>
2014-11-03 22:59:53 +0800

    add lucene

---
+++ hecjlucene-core/src/test/java/org/test/hecjlucene/core06_analyzer/AnalyzerUtil.java
@@ -0,0 +1,51 @@
+package org.test.hecjlucene.core06_analyzer;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
+import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
+
+public class AnalyzerUtil {
+	
+	public static void displayToken(String str,Analyzer a){
+		
+		TokenStream tokenStream = a.tokenStream("content", new StringReader(str));
+		try {
+			CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+			while(tokenStream.incrementToken()){
+				System.out.print(" ["+charTermAttribute+"]");
+			}
+			System.out.println();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void displayAllTokenInfo(String str,Analyzer a){
+		
+		TokenStream stream = a.tokenStream("content", new StringReader(str));
+		//位置增量的属性，存储语汇元之间的距离
+		PositionIncrementAttribute pia = stream.addAttribute(PositionIncrementAttribute.class);
+		//每个语汇单元的位置偏移量
+		OffsetAttribute oa = stream.addAttribute(OffsetAttribute.class);
+		//存储每一个语汇单元的信息（分词单元信息）
+		CharTermAttribute cta = stream.addAttribute(CharTermAttribute.class);
+		//使用的分词器的类型信息
+		TypeAttribute ta = stream.addAttribute(TypeAttribute.class);
+		try {
+			while(stream.incrementToken()){
+				System.out.print(pia.getPositionIncrement()+":");
+				System.out.print(cta+"["+oa.startOffset()+"-"+oa.endOffset()+"]-->"+ta.type()+"\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
---
+++ hecjlucene-core/src/test/java/org/test/hecjlucene/core06_analyzer/TestAnalyzerUtil.java
@@ -0,0 +1,67 @@
+package org.test.hecjlucene.core06_analyzer;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.SimpleAnalyzer;
+import org.apache.lucene.analysis.StopAnalyzer;
+import org.apache.lucene.analysis.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.util.Version;
+import org.junit.Test;
+
+public class TestAnalyzerUtil {
+
+	@Test
+	public void test01() {
+		Analyzer analyzer1 = new StandardAnalyzer(Version.LUCENE_35);
+		Analyzer analyzer2 = new StopAnalyzer(Version.LUCENE_35);
+		Analyzer analyzer3 = new SimpleAnalyzer(Version.LUCENE_35);
+		Analyzer analyzer4 = new WhitespaceAnalyzer(Version.LUCENE_35);
+
+		String txt = "this is my house,I am come from yunnang zhaotong,"
+				+ "My email is ykfds@fd.com,"
+				+ "My QQ IS 3453434";
+		
+		AnalyzerUtil.displayToken(txt, analyzer1);
+		AnalyzerUtil.displayToken(txt, analyzer2);
+		AnalyzerUtil.displayToken(txt, analyzer3);
+		AnalyzerUtil.displayToken(txt, analyzer4);
+	}
+	/**
+	 * @函数功能说明 经测试不支持中文文词
+	 * @修改作者名字 JECJ  
+	 * @修改时间 2014年11月3日
+	 * @修改内容
+	 * @参数：     
+	 * @return void   
+	 * @throws
+	 */
+	@Test
+	public void test02() {
+		Analyzer analyzer1 = new StandardAnalyzer(Version.LUCENE_35);
+		Analyzer analyzer2 = new StopAnalyzer(Version.LUCENE_35);
+		Analyzer analyzer3 = new SimpleAnalyzer(Version.LUCENE_35);
+		Analyzer analyzer4 = new WhitespaceAnalyzer(Version.LUCENE_35);
+		
+		String txt = "我来自安徽省界首市！";
+		
+		AnalyzerUtil.displayToken(txt, analyzer1);
+		AnalyzerUtil.displayToken(txt, analyzer2);
+		AnalyzerUtil.displayToken(txt, analyzer3);
+		AnalyzerUtil.displayToken(txt, analyzer4);
+	}
+	@Test
+	public void test03() {
+		Analyzer analyzer1 = new StandardAnalyzer(Version.LUCENE_35);
+		Analyzer analyzer2 = new StopAnalyzer(Version.LUCENE_35);
+		Analyzer analyzer3 = new SimpleAnalyzer(Version.LUCENE_35);
+		Analyzer analyzer4 = new WhitespaceAnalyzer(Version.LUCENE_35);
+		
+		String txt = "how are you thank you";
+		
+		AnalyzerUtil.displayAllTokenInfo(txt, analyzer1);
+		AnalyzerUtil.displayAllTokenInfo(txt, analyzer2);
+		AnalyzerUtil.displayAllTokenInfo(txt, analyzer3);
+		AnalyzerUtil.displayAllTokenInfo(txt, analyzer4);
+	}
+
+}
